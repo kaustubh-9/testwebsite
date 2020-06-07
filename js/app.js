a2fae492f7479d5ed2e3e07803000cd57366c5ea
kaustubh <63561984+kaustubh-9@users.noreply.github.com>
2020-06-07 14:09:30 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,12 @@
+
+function toggle(){
+
+  var navbar=document.getElementById("nav-links")
+
+  if (navbar.style.display=="block"){
+    navbar.style.display="none";
+  }
+  else{
+    navbar.style.display="block";
+  }
+}
